2ae8feaaea8b5d6dc850451b20477036d52a66ab
Artak Harutyunyan <artak_harutyunyan@mail.com>
2020-09-11 22:03:03 +0400

    Creating pieces and board

---
+++ src/pieces/king.js
@@ -0,0 +1,28 @@
+import Piece from "./piece.js";
+
+export default class King extends Piece {
+  constructor(player) {
+    super(
+      player,
+      player === 1
+        ? "https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg"
+        : "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg"
+    );
+  }
+
+  isMovePossible(src, dest) {
+    return (
+      src - 9 === dest ||
+      src - 8 === dest ||
+      src - 7 === dest ||
+      src + 1 === dest ||
+      src + 9 === dest ||
+      src + 8 === dest ||
+      src + 7 === dest ||
+      src - 1 === dest
+    );
+  }
+  getSrcToDestPath(src, dest) {
+    return [];
+  }
+}
