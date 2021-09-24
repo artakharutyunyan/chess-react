0df97b1025a77153a6a15f19debee432b8592793
Artak Harutyunyan <artak_harutyunyan@mail.com>
2021-09-24 18:22:21 +0400

    Layout fixes

--- src/i18n/translations/en.js
+++ src/i18n/translations/en.js
@@ -6,7 +6,7 @@ export const en = {
       play: "Play",
     },
     championsList: {
-      header: "Chess Immortals: The World Champions of Chess",
+      header: "The World Champions of Chess",
       text: "The classical line of World Chess Champions began with Wilhelm Steinitz's defeat of Johannes Zukertort in their 1886 match. Since that time, the World Championship has typically been contested in a match between the current champion and a challenger, though tournaments have been used on occasion for various reasons.",
       steinitz: "Wilhelm Steinitz",
       lasker: "Emanuel Lasker",
